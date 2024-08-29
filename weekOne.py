bf792adbaf95faca879812c2b98664d67ad73de2
Cloud1590 <daniel4vance@gmail.com>
2024-08-29 11:58:04 -0500

    Create weekOne.py

---
+++ weekOne.py
@@ -0,0 +1,67 @@
+'''
+print("Hello World")
+print("I like chicken")
+print(40 + 40)
+age = 18
+print(age)
+print(age + 1)
+
+str_welcome = 'welcome to BU sports.'
+
+print(str_welcome)
+
+firstName = "Daniel"
+lastName = "Vance"
+Major = "Computer Information Systems & Cybersecurity"
+#age = "18"
+
+#Concatenaton
+print(firstName + " " + lastName + ", " + Major)
+
+x, y, z, = 0, 1, 2
+print(x)
+print(y)
+print(z)
+
+
+value = 3
+chicken = value
+print(chicken)
+
+lunch = 0
+
+lunch = input("What would you like for lunch? ")
+
+
+
+print("You have chosen " + lunch + " for lunch.")
+
+'''
+
+
+
+
+'''
+mySalary=input("Enter desired salary ")
+print(mySalary)
+
+myRaise=int(mySalary)+10000
+print(myRaise)
+'''
+
+
+#mycost = 10.49
+#myprint = float(mycost)
+#print(myprint)
+
+mysalary=input("what is your starting salary? ")
+print(mysalary)
+
+
+mynewsalary = int(mysalary) + 10000
+
+#mystring = str(mysalary)
+print(mynewsalary)
+
+
+
