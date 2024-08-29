4494a982ebf9a3d99584c99d28803dd9a4202f77
Cloud1590 <daniel4vance@gmail.com>
2024-08-29 17:35:53 -0500

    Add weeek one assignment (Fiber-Cost Calculator)

---
+++ weekOneAssignment.py
@@ -0,0 +1,14 @@
+#Display welcome message to the console
+print("Welcome to the Fiber-Cost Calculator!")
+
+#Get the length (In Feet) of fiber needed from the user
+fiberLength = input("Enter the length of fiber cable needed in feet: ")
+
+#Assign the cost of fiber per foot to a variable
+fiberCost = 0.87
+
+#Calculate the total cost of fiber
+totalCost = int(fiberLength) * fiberCost
+
+#Display the total cost of fiber to the console
+print("The total cost of fiber is $" + str(totalCost))
