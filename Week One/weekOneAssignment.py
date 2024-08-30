7f41e19626aab7a06234c66b9b6c44c584a4b788
Cloud1590 <daniel4vance@gmail.com>
2024-08-29 22:16:25 -0500

    fixed issue with formatting money in rare cases

--- Week One/weekOneAssignment.py
+++ Week One/weekOneAssignment.py
@@ -10,7 +10,28 @@ fiberCost = 0.87
 #Calculate the total cost of fiber
 totalCost = float(fiberLength) * fiberCost
 
-#Display the company name and total cost of fiber to the console
 
+
+#print("SpeedForce Fiber")
+#print("The total cost of fiber is $" + str(totalCost))
+
+'''
+the above commented code is what I had before I found out about the f-string formatting
+to fix the output to two decimal places in some cases where the dollar amount
+does not have two decimal places. 
+
+example of this:
+total length of fiber = 50
+output would be 43.5 instead of 43.50
+
+I think this should be addressed briefly during class at some point. 
+I helped Michael with this issue because he ran into it as well.
+'''
+
+# Formatting the output string to include two decimal places
 print("SpeedForce Fiber")
-print("The total cost of fiber is $" + str(totalCost))
+print(f"The total cost of fiber is ${totalCost:.2f}")
+
+
+
+
