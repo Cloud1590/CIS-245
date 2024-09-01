63d2c2b0093a9f4e43ef67dfb5f442ad988f0a01
Cloud1590 <daniel4vance@gmail.com>
2024-08-31 23:06:04 -0500

    Create DB_Example.py

---
+++ DB_Example.py
@@ -0,0 +1,11 @@
+#explain how to combine two strings in the print statement
+
+print("My name is" + " " + "Michael.")
+
+
+
+num1 = 420
+num2 = 69
+
+
+print("the sum of " + str(num1) + " and " + str(num2) + " is " + str(num1 + num2) + ".")
