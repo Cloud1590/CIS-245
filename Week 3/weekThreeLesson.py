ff2c3cd3f7587dcfd195edbd1ffd10b4f915cd01
Cloud1590 <daniel4vance@gmail.com>
2024-09-09 12:06:43 -0500

    Refactor while loop to display numbers 1-10 and add comments explaining the difference between while loop and while true loop, and while loop and for loop.

--- Week 3/weekThreeLesson.py
+++ Week 3/weekThreeLesson.py
@@ -3,10 +3,28 @@ num = 1
 while num <= 10:
     print(num)
     num += 1  
+
 # Output: 1 2 3 4 5 6 7 8 9 10
 #identify the initialization, comparison, and update expressions in the while loop
 # initialization: num = 1
 # comparison: num <= 10
+
+for x in 4, 5, 6, 7, 8, 9, 10:
+    print(x)
+
+
+for x in range(2, 11):
+    print(x)
+
+
+
+
+
+
+
+
+
+
 # ask if there's a difference between while loop and while true loop
 # while loop: executes as long as the condition is true
 # while true loop: executes indefinitely
