aa7ae5b48bcd6cf80f8653bbf86e3fea4360693f
Cloud1590 <daniel4vance@gmail.com>
2024-09-16 11:08:27 -0500

    Added Week Four Files

---
+++ Week 4/weekFourLesson.py
@@ -0,0 +1,9 @@
+# Functions!
+
+#int() - converts a string to an integer
+#float() - converts a string to a float
+#str() - converts a number to a string
+#input() - gets input from the user
+#Print() - prints to the console
+#input() - gets input from the user
+
