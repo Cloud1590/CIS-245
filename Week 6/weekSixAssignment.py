dd774bb37023c67f0ae7a4378d9cd9ac6fbad0af
Cloud1590 <daniel4vance@gmail.com>
2024-10-01 11:04:42 -0500

    Add weekSixAssignment.py with array and customer product verification examples

---
+++ Week 6/weekSixAssignment.py
@@ -0,0 +1,75 @@
+# arrays
+
+temp = []
+temp.append(34)
+temp.append(78)
+
+for x in temp:
+    print(x)
+    
+for x in range(len(temp)):
+    print(temp[x])
+    
+
+
+'''
+
+# list can have multiple data types
+
+my_list = []
+my_list.append("The correct answer is ")
+my_list.append("80")
+
+for x in my_list:
+    print(x)
+    
+    
+# using the range()
+
+for x in range(2):
+    print(x)
+
+'''
+
+
+'''
+
+my_list = []
+my_list.append("The correct answer is ")
+my_list.append("80")
+
+my_second_list = []
+my_second_list.append("On the test")
+
+x = my_list + my_second_list
+print(x)
+print(len(x))
+print(x [0:2])
+
+'''
+
+# Customer claims product purchased did not arrive. first thing needed to be determined is if the product purchased the product. 
+
+customer_name = input("Enter customer name: ")
+product_purchased = input("Enter product purchased: ")
+
+file = open("customer.txt", "r")
+my_products = [] # empty list
+
+for x in file:
+    my_products.append(x)
+
+if product_purchased in my_products:
+    print("Product purchased")
+    
+else:
+    print("Product not purchased")
+    
+# could we do this without a file? - no. 
+# coukd we do this without a list? - yes.
+
+
+
+
+
+
